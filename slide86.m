dc84de431fe9d7e1fb9d1aa97bd9bd456fc9607b
Tarek Allam <t.allam.jr@gmail.com>
2016-02-03 16:50:29 +0000

    Added original code from instrcutions given on slide 86 of notes

---
+++ slide86.m
@@ -0,0 +1,49 @@
+%read in the results data - you need both the text elements and the numeric elements
+
+[]=xlsread()
+
+%extract the maths, english and history data into vectors
+%this data is in the numeric part of the result of xslread
+
+maths=
+english=
+history=
+
+%find the length of each these vectors - this is the N, the number of observations in the data
+%you can get it be examining just *one* vector
+dims=size()
+n=dims()
+
+%display the value of N
+%use fprintf
+%this works by replacing variables in the string with a format character and then listing the variable names
+%*after* the string.  The format characters and variables are then interpolated in order:
+
+%c Single character
+%d Decimal notation (signed)
+%e Exponential notation (using a lowercase e as in 3.1415e+00)
+%E Exponential notation (using an uppercase E as in 3.1415E+00)
+%f  Fixed-point notation
+%g  The more compact of %e or %f. (Insignificant zeros do not print.)
+%G Same as %g, but using an uppercase E
+%o Octal notation (unsigned)
+%s String of characters
+%u Decimal notation (unsigned)
+%x Hexadecimal notation (using lowercase letters a–f)
+%X Hexadecimal notation (using uppercase letters A–F)
+
+%so
+%fprintf('The number of observations is %d',x)
+%would replace %d in the string with the value of x and display it as a signed decimal
+
+%find the mean maths result by summing maths and dividing by n
+
+meanmaths= sum()/m;
+
+%display "the value of the mean is...."
+
+%find the mean examination score (ie for english, history and maths tests) for the first pupil in your data
+
+%display "the first pupil had a mean test score of..."
+
+%rest, relax
